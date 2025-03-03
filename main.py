bea91c4cc50916a617a8e381af9b5075ce7c792b
Marcus <marcusphan1234@gmail.com>
2025-03-03 12:25:32 -0500

    Update register.html
    Small changes to the file

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import MySQLdb.cursors, re, hashlib
 
 app = Flask(__name__)
 
-# Dummy user data for login
+app.secret_key = 'cheese'
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'mental_health_db'
+
+mysql = MySQL(app)
+
+#Dummy user data for login
 USER_CREDENTIALS = {
-    "test@example.com": "password123"
+   "test@example.com": "password123"
 }
 
 @app.route("/", methods=["GET", "POST"])
 def login():
     error = None
-    if request.method == "POST":
+    if request.method == "POST" and 'email' in request.form and 'password' in request.form:
         email = request.form["email"]
         password = request.form["password"]
+
+        hash = password + app.secret_key
+        hash = hashlib.sha1(hash.encode())
+        password = hash.hexdigest()
         
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email,password,))
+        users = cursor.fetchone()
+
+        if users:
+            session['loggedin'] = True
+            session['id'] = users['id']
+            session['email'] = users['email']
+            return redirect(url_for("home"))
+        else:
+            return 'Incorrect Email/Password'
+
         if email in USER_CREDENTIALS and USER_CREDENTIALS[email] == password:
             return redirect(url_for("home"))  # Redirect on successful login
         else:
@@ -21,9 +49,21 @@ def login():
 
     return render_template("index.html", error=error)
 
+@app.route('/register', methods = ['GET','POST'])
+def register():
+    if request.method =='POST' and 'name' in request.form and 'email' in request.form and 'password' in request.form:
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+    elif request.method == 'POST':
+        return "Please Fill out the form"
+    
+    return render_template('register.html')
+
 @app.route("/home")
 def home():
     return render_template('home.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
